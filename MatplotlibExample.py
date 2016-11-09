4d5da9aabf8a49c1d2cb3c5a81777665fe39d459
KatCherryfarmer <katharina.kirschbauer@st.oth-regensburg.de>
2016-11-09 20:34:36 +0000

    basic version now of our future developments

--- MatplotlibExample.py
+++ MatplotlibExample.py
@@ -1,73 +1,85 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
 import csv
 import CSVLoad
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
 
-# Open the earthquake data file.
+
+#-----------------------------
+#values
 filename = 'earthquake.csv'
-
-# Create empty lists for the data we are interested in.
 lats, lons = [], []
-magnitudes = []
-timestrings = []
+mags = []
+times = []
 
-# Read through the entire file, skip the first line,
-#  and pull out just the lats and lons.
+
+#------------------------------
+#get values from file
 with open(filename) as f:
-    # Create a csv reader object.
-    reader = csv.reader(f)
+    reader = csv.reader(f, delimiter=',')
+    next(reader, None)  # skip the headers
 
-    # Ignore the header row.
-    next(reader)
-
-    # Store the latitudes and longitudes in the appropriate lists.
     for row in reader:
-        lats.append(float(row[1]))
-        lons.append(float(row[2]))
-        magnitudes.append(float(row[4]))
-        timestrings.append(row[0])
+        if row:
+            lats.append(float(row[1]))
+            lons.append(float(row[2]))
+            mags.append(float(row[4]))
+            times.append(row[0])
 
-# --- Build Map ---
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
 
-def get_marker_color(magnitude):
-    # Returns green for small earthquakes, yellow for moderate
-    #  earthquakes, and red for significant earthquakes.
-    if magnitude < 3.0:
+#------------------------------
+#colorize
+def get_marker_color(mags):
+    if mags < 3.0:
         return ('go')
-    elif magnitude < 5.0:
+    elif mags < 5.0:
         return ('yo')
     else:
         return ('ro')
 
-
-eq_map = Basemap(projection='robin', resolution='l', area_thresh=1000.0,
-                 lat_0=0, lon_0=-130)
-eq_map.drawcoastlines()
-eq_map.drawcountries()
-# eq_map.fillcontinents(color = 'gray')
-# eq_map.bluemarble()
-eq_map.drawmapboundary()
-#eq_map.drawmeridians(np.arange(0, 360, 30))
-#eq_map.drawparallels(np.arange(-90, 90, 30))
-
-min_marker_size = 2.25
-for lon, lat, mag in zip(lons, lats, magnitudes):
-    x, y = eq_map(lon, lat)
-    msize = mag * min_marker_size
-    marker_string = get_marker_color(mag)
-    eq_map.plot(x, y, marker_string, markersize=msize)
-
-title_string = "Earthquakes of Magnitude 1.0 or Greater\n"
-title_string += "%s through %s" % (timestrings[-1][:10], timestrings[0][:10])
-plt.title(title_string)
-
-ax = plt.axes([0,0.9,0.1,0.1])
-
+#------------------------------
+#load file button
 def buttonOnClicked(event):
     CSVLoad.openFileDialogCsv()
 
-b = plt.Button(ax, "Load .csv")
+
+#------------------------------
+#build map
+fig = plt.figure()
+title_string = "Earthquakes of Magnitude 1.0 or Greater\n"
+title_string += "%s through %s" % (times[-1][:10], times[0][:10])
+plt.title(title_string)
+
+map = Basemap()
+#map = Basemap(projection='robin',lon_0=0,resolution='c') #plt.title("Robinson Projection")
+
+map.etopo() #'contour'
+#map.bluemarble() #'satellite'
+
+# draw coastlines, parallels and meridians.
+map.drawcoastlines()
+map.drawparallels(np.arange(-90.,120.,5.))
+map.drawmeridians(np.arange(0.,360.,5.))
+
+
+#------------------------------
+#size/color marking points
+msize = 9
+for lon, lat, mag in zip(lons, lats, mags):
+    x, y = map(lon, lat)
+    #msize = mag * min_marker_size
+    marker_string = get_marker_color(mag)
+    map.plot(x, y, marker_string, markersize=msize)
+
+ax = plt.axes([0,0.9,0.1,0.1])
+
+#------------------------------
+#button load file
+b = plt.Button(ax, "Load")
 b.on_clicked(buttonOnClicked)
+
 
 plt.show()
 
